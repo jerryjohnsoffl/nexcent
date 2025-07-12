b07c4ec17f742ce996853522797db68ecf782a22
jerryjohnsoffl <jerryjohnsoffl@gmail.com>
2025-07-12 22:18:21 +0530

    Text centered

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import Header from "./components/Header"
+import Intro from './components/Intro'
+import Client from './components/Client'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Header />
+      <Intro />
+      <Client />
     </>
   )
 }
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -6,7 +6,7 @@ const Intro = () => {
     <div className="flex flex-col-reverse items-center bg-silver p-5 md:flex-row md:justify-around h-full">
         <div className="container flex flex-col items-center my-5 p-5 md:w-1/2 md:items-start md:my-28">
             <h1 className='text-6xl my-2 md:text-7xl text-center md:text-left'>Lessons and insights</h1>
-            <h1 className='text-6xl my-2 md:text-7xl'>from 8 years</h1>
+            <h1 className='text-6xl text-center my-2 md:text-7xl'>from 8 years</h1>
             <p className='my-2 text-center md:text-left text-grey'>Where to grow your business as a photographer: site or social media?</p>
             <button className="rounded-md text-white bg-green px-5 py-3">Register</button>
         </div>
