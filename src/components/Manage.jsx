7a219506c48c20a6b29727e2d8659199622a1190
jerryjohnsoffl <jerryjohnsoffl@gmail.com>
2025-07-25 23:21:58 +0530

    Added manage section

--- src/components/Manage.jsx
+++ src/components/Manage.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Manage = () => {
   return (
     <div className='py-5 flex justify-center'>
-      <div className="w-1/2">
-        <h1 className="text-4xl mx-3 leading-14 sm:text-5xl font-semibold text-dgrey text-center">Manage your entire community in a single system</h1>
+      <div className="w-full md:w-10/12 lg:w-1/2">
+        <h1 className="text-4xl mx-3 leading-14 md:text-5xl font-semibold text-dgrey text-center">Manage your entire community in a single system</h1>
         <p className="text-center my-5 text-grey">Who is Nextcent suitable for?</p>
       </div>
     </div>
